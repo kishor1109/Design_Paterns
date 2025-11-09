2e76127857fbdcb3c8079b474a18143762aa43a4
kishor1109 <1109jahan@gmail.com>
2025-11-09 10:02:05 +0530

    git add

--- src/Octagon.java
+++ src/Octagon.java
@@ -1,7 +1,13 @@
+// The Octagon class implements the Polygon interface
+// This means Octagon must provide (implement) all methods defined in Polygon
 public class Octagon implements Polygon {
 
+    // This method is an implementation of a method declared in the Polygon interface
+    // It prints a message saying "Draw an Octagon" when called
+    // and returns an empty string (since the return type is String)
+    @Override
     public String getType() {
         System.out.println("Draw a Octagon");
-        return "";
+        return "Octagon";
     }
 }
--- src/Pentagon.java
+++ src/Pentagon.java
@@ -1,9 +1,13 @@
+// The Pentagon class implements the Polygon interface
+// This means that Pentagon must define all methods declared in the Polygon interface.
 public class Pentagon implements Polygon {
 
+    // This method overrides (implements) a method from the Polygon interface.
+    // It prints a message indicating that a Pentagon is being drawn.
+    // Then, it returns an empty string (though returning the shape name would be more useful).
+    @Override
     public String getType() {
         System.out.println("Draw a Pentagon");
-
-
-        return "";
+        return "Pentagon";
     }
 }
